b1750c7619a2414b8b49e211562237a99e7bb030
Priyankam <mohantypriyanka015@gmail.com>
2018-08-02 12:22:15 +0530

    comiit php file

--- techMPost.php
+++ techMPost.php
@@ -1,8 +1,9 @@
 <?php
 //$jsonurl = "http://api.wipmania.com/json";
 //$jsonurl = "http://www.mocky.io/v2/5b615a19300000ef0d6a4087";
-//$jsonurl = "http://www.mocky.io/v2/5b618020300000d8046a416e";
-$jsonurl = "http://www.mocky.io/v2/5b618026300000d8046a4170";
+//$jsonurl = "http://www.mocky.io/v2/5b61a85e300000221e6a4281";
+$jsonurl = "http://www.mocky.io/v2/5b61a5d7300000221e6a4274";
+//$jsonurl = "http://www.mocky.io/v2/5b62a5033000005200650045";
 $json = file_get_contents($jsonurl);
 //echo $json;
 $jsonValue=json_decode($json);
